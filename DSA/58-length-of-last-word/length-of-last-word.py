43f52ac1234a81298054545e77c20526f52903ff
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-06-26 22:50:35 +0530

    Time: 32 ms (75.83%) | Memory: 16.4 MB (92.38%) - LeetSync

--- DSA/58-length-of-last-word/length-of-last-word.py
+++ DSA/58-length-of-last-word/length-of-last-word.py
@@ -2,9 +2,18 @@ class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         count = 0
         for i in s[::-1]:
-            if i ==" " and count != 0:
-                break
-            if i != " ":
-                print(i)
-                count+=1        
+            if i ==" ":
+                if count != 0:
+                    break
+                continue
+            count+=1        
         return count
+
+        # count = 0
+        # for i in range(len(s)-1,-1,-1):
+        #     if s[i] == ' ':
+        #         if count > 0:
+        #             break
+        #         continue
+        #     count += 1
+        # return count
