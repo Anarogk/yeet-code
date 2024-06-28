6f01a20e44e0a8b46d9c0739687c0f95462f371d
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-06-28 13:48:26 +0530

    Time: 49 ms (77.74%) | Memory: 16.5 MB (33.70%) - LeetSync

--- DSA/3226-minimum-number-game/minimum-number-game.py
+++ DSA/3226-minimum-number-game/minimum-number-game.py
@@ -1,9 +1,7 @@
 import heapq
 class Solution:
     def numberGame(self, nums: List[int]) -> List[int]:
-        nums_h = nums
         heapq.heapify(nums)
-        print(nums)
         arr = []
         while nums:
             item = heapq.heappop(nums)
