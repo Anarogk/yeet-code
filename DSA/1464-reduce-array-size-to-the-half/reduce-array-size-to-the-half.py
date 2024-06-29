d903e0779f9b6ad6c4c9179790c95e3de11ba288
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-06-29 12:45:18 +0530

    Time: 429 ms (69.15%) | Memory: 34.7 MB (64.27%) - LeetSync

--- DSA/1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
+++ DSA/1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
@@ -2,10 +2,8 @@ import heapq
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
         cn = Counter(arr)
-        print(cn)
         cn = [-value for value in cn.values()]
         heapq.heapify(cn)
-        
         amount = len(arr)//2 + len(arr) % 2
         counter = 0
         while amount > 0:
