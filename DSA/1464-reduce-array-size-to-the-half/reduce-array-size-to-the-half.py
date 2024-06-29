6b5ea379b5b8b1ea7faa7da9eae1947ea7fc117a
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-06-29 12:53:11 +0530

    Time: 405 ms (96.72%) | Memory: 34.1 MB (92.64%) - LeetSync

--- DSA/1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
+++ DSA/1464-reduce-array-size-to-the-half/reduce-array-size-to-the-half.py
@@ -5,17 +5,15 @@ class Solution:
         freq = sorted(cn.values(), reverse=True)
         rem = 0
         nums = 0
-
         for i in freq:
             rem +=i
             nums += 1
             if rem >= len(arr)//2:
-                break
-        return nums
+                return nums
+        
         # heapq.heapify(cn)
         # amount = len(arr)//2 + len(arr) % 2
         # counter = 0
         # while amount > 0:
         #     amount += heapq.heappop(cn)
         #     counter += 1
-        return counter
