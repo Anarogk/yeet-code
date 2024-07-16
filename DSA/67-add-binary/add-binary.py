aa565763a9aa32b29b798343e5f3c173e5d700f2
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-07-16 06:18:58 +0530

    Time: 41 ms (34.14%) | Memory: 16.6 MB (22.02%) - LeetSync

---
+++ DSA/67-add-binary/add-binary.py
@@ -0,0 +1,16 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        num, num2 = 0,0
+        for j,i in enumerate(a[::-1]):
+            num += int(i) * 2**j
+        for j,i in enumerate(b[::-1]):
+            num += int(i) * 2**j
+        s = ""
+        while num>0:
+            s = str(num%2) + s
+            num = num//2
+        if a == "0" and b=="0":
+            return "0"
+            
+        return s
