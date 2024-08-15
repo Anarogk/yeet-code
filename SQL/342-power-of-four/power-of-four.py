26c8152fb543dd1fa1067367fbcf1f179ff2fce2
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-15 16:14:46 +0530

    Time: 27 ms (96.51%) | Memory: 16.6 MB (31.77%) - LeetSync

--- SQL/342-power-of-four/power-of-four.py
+++ SQL/342-power-of-four/power-of-four.py
@@ -1,8 +1,9 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        arr= []
-        for i in range(100):
-            arr.append(4**i)
-        if n in arr:
-            return True
-        return False
+        if n<= 0:
+            return False
+        while n > 1:
+            if n % 4 != 0:
+                return False
+            n /= 4
+        return True
