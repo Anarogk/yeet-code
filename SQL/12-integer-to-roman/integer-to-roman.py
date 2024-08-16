acf890cd3a8154419c185dc5334535b3a1e8cda7
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-17 00:52:31 +0530

    Time: 37 ms (94.15%) | Memory: 16.5 MB (85.92%) - LeetSync

--- SQL/12-integer-to-roman/integer-to-roman.py
+++ SQL/12-integer-to-roman/integer-to-roman.py
@@ -1,13 +1,14 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        nums = ["M","CM", "D","CD", "C","XC","L","XL","X","IX","V","IV","I"]
-        imp = [1000, 900, 500, 400,100, 90, 50, 40, 10,9,5,4,1 ]
-        st= ""
-        i =0
-        while num>0 and i < len(imp):
-            if num >= imp[i]:
-                num =num-imp[i]
-                st+= nums[i]
-            else:
-                i+=1
-        return st
+        d = {
+        1000: "M", 900:"CM",500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X",
+        9:"IX", 5:"V", 4:"IV",  1:"I"
+    }
+
+        res =""
+        for i, n in d.items():
+            while num>=i:
+                res +=n
+                num -=i
+
+        return res
