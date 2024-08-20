39ad9d4e2768729ae373b759ea7d00329b1ce63c
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-20 13:33:29 +0530

    Time: 55 ms (81.02%) | Memory: 17.7 MB (28.62%) - LeetSync

--- DSA/1-two-sum/two-sum.py
+++ DSA/1-two-sum/two-sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapp = {nums[0]:0}
-        for i in range(1,len(nums)):
-            if target - nums[i] in mapp.keys():
-                return [mapp.get(target -nums[i]), i]
-            mapp[nums[i]] = i
+        mapp = {nums[0]:0} # add first item to map with index
+        for i, num in enumerate(nums):
+            if target - num in mapp.keys() and i not in mapp.values():
+                return [mapp.get(target -num), i]
+            mapp[num] = i
         return []
     
                 
