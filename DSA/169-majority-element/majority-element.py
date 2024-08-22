7f88da001d65605d5e9bc01c93d25bbb577dc573
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-22 21:45:50 +0530

    Time: 166 ms (76.10%) | Memory: 18 MB (20.05%) - LeetSync

--- DSA/169-majority-element/majority-element.py
+++ DSA/169-majority-element/majority-element.py
@@ -1,15 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums)//2
-        count =0
-        dic = dict()
-        for i in nums:
-            if i in dic.keys():
-                dic[i] +=1
-            else:
-                dic[i] = 1
-        for i,j in dic.items():
-            if j> n:
-                return i
-
-        return count
+        nums.sort()
+        n = len(nums) // 2
+        return nums[n]
