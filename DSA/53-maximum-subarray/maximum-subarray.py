b94dbe564803ef11fc62cb2608ba46b81904439e
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-24 21:10:02 +0530

    Time: 472 ms (98.80%) | Memory: 31 MB (39.28%) - LeetSync

--- DSA/53-maximum-subarray/maximum-subarray.py
+++ DSA/53-maximum-subarray/maximum-subarray.py
@@ -1,10 +1,21 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
-        curr_sum = 0
 
+        max_sum = float('-inf')
+        curr_sum = 0
+        
         for i in nums:
-            curr_sum = max(curr_sum, 0)
             curr_sum +=i
-            max_sum = max(curr_sum , max_sum)
+            if max_sum<curr_sum:
+                max_sum = curr_sum
+            if curr_sum < 0:
+                curr_sum = 0
         return max_sum
+        # max_sum = nums[0]
+        # curr_sum = 0
+
+        # for i in nums:
+        #     curr_sum = max(curr_sum, 0)
+        #     curr_sum +=i
+        #     max_sum = max(curr_sum , max_sum)
+        # return max_sum
