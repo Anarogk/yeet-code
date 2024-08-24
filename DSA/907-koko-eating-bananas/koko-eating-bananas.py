4e47f60ab586806194c163a7319a39f6a4205316
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-24 15:06:20 +0530

    Time: 251 ms (87.28%) | Memory: 18.3 MB (14.05%) - LeetSync

--- DSA/907-koko-eating-bananas/koko-eating-bananas.py
+++ DSA/907-koko-eating-bananas/koko-eating-bananas.py
@@ -2,8 +2,6 @@ class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         def isSuff(cnt):
             return sum(ceil(i/cnt)for i in piles)<=h
-        if h== len(piles):
-            return max(piles)
         l , r = 1, max(piles)
         while l<r:
             m = (l+r)//2
