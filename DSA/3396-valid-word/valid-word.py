2af057aef683d3b239c5d20f570f1ce56b9c316c
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-25 18:32:34 +0530

    Time: 35 ms (66.94%) | Memory: 16.6 MB (7.59%) - LeetSync

--- DSA/3396-valid-word/valid-word.py
+++ DSA/3396-valid-word/valid-word.py
@@ -1,14 +1,16 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        vowel , consonant = 0,0
-        if len(word) <3:
+        if len(word) < 3:
             return False
-        if '@' in word or '#' in word or '$' in word:
-            return False
-        for i in range(len(word)):
-            if word[i].isalpha():
-                if word[i] in ['a', 'e', 'i', 'o', 'u'] or word[i] in ['A', 'E', 'I', 'O', 'U']:
-                    vowel += 1
-                else:
-                    consonant += 1
-        return True if (vowel != 0 and consonant != 0) else False
+        vowels = set("aeiouAEIOU")
+        has_vowel = False
+        has_consonant = False
+        for char in word:
+            if char.isdigit() or char.isalpha():
+                if char in vowels:
+                    has_vowel = True
+                elif char.isalpha():
+                    has_consonant = True
+            else:
+                return False 
+        return has_vowel and has_consonant
