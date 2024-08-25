c49c1522c291e87b81da419a766011089ebc0d59
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-26 01:50:11 +0530

    Time: 35 ms (51.16%) | Memory: 16.4 MB (57.80%) - LeetSync

--- DSA/278-first-bad-version/first-bad-version.py
+++ DSA/278-first-bad-version/first-bad-version.py
@@ -4,7 +4,6 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
         left, right = 1, n
-
         while left < right:
             version = left + (right - left) // 2 
             if isBadVersion(version):
