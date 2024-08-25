56a6d1d30e324c824e5906e7a492b01707d90b7d
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-25 11:07:13 +0530

    Time: 32 ms (74.12%) | Memory: 16.5 MB (72.34%) - LeetSync

--- DSA/394-decode-string/decode-string.py
+++ DSA/394-decode-string/decode-string.py
@@ -1,6 +1,22 @@
-import re
+
 class Solution:
     def decodeString(self, s: str) -> str:
-        while (m:= re.search(r"([0-9]+)\[([a-z]+)\]", s)):
-            s = s.replace(m.group(), f"{m[2]* int(m[1])}")
-        return s
+        # while (m:= re.search(r"([0-9]+)\[([a-z]+)\]", s)):
+        #     s = s.replace(m.group(), f"{m[2]* int(m[1])}")
+        # return s
+        st = []; curNum = 0; curStr = ''
+        for c in s:
+            if c.isdigit():
+                curNum = curNum * 10 + int(c)
+            elif c == '[':
+                st.append(curStr)
+                st.append(curNum)
+                curStr = ''
+                curNum = 0
+            elif c == ']':
+                preNum = st.pop()
+                prevStr = st.pop()
+                curStr = prevStr + curStr * preNum
+            else:
+                curStr += c
+        return curStr
