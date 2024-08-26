91690c6187fa19f77cbd03022798db4c933db79b
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-26 19:47:32 +0530

    Time: 32 ms (75.19%) | Memory: 16.6 MB (20.35%) - LeetSync

--- DSA/812-rotate-string/rotate-string.py
+++ DSA/812-rotate-string/rotate-string.py
@@ -1,11 +1,8 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        s = list(s)
         for i in range(len(s)):
-            temp = s[0]
-            for i in range(len(s)-1):
-                s[i] = s[i+1]
-            s[-1] = temp
-            if s == list(goal):
+            a = s[0]
+            s = s[1:] + a
+            if s == goal:
                 return True
         return False
