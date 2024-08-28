6740c34b1fc4ab1972b4f68e3dfff368d7c4126f
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-28 22:03:23 +0530

    Time: 30 ms (87.48%) | Memory: 16.6 MB (50.47%) - LeetSync

--- DSA/20-valid-parentheses/valid-parentheses.py
+++ DSA/20-valid-parentheses/valid-parentheses.py
@@ -1,11 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack = []
-        mapping = {")": "(", "}": "{", "]": "["}
+        stack = []  # created stack for opened brackets
+        mapping = {")": "(", "}": "{", "]": "["}  # created map
         for i in s:
-            if i in mapping.values():
+            if i in mapping.values():  # if i is opener we append to stack
                 stack.append(i)
+            # if i is closer we see if stack has opened that exact bracket
             elif i in mapping.keys():
+                # if stack has that same exact bracket open close it
                 if not stack or mapping.get(i) != stack.pop():
+                    # if not opened any bracket or that same exact bracket then return False
                     return False
         return not stack
+
