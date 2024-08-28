33c86ea7d8043877fedeff2145470cec884bc71b
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-08-28 13:08:31 +0530

    Time: 550 ms (7.72%) | Memory: 29.3 MB (81.91%) - LeetSync

--- DSA/11-container-with-most-water/container-with-most-water.py
+++ DSA/11-container-with-most-water/container-with-most-water.py
@@ -1,15 +1,31 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maximum = float("-inf")  # set max to -inf
-        i, j = 0, len(height) - 1  # initialized 2 pointers at end of array
-
-        while i < j:  # loop through height array
-            # we select min of both -- because overflow
+        maxx = float('-inf')
+        i, j = 0, len(height)-1
+        while i <j:
             min_height = min(height[i], height[j])
-            vol = (j - i) * min_height  # vol =
-            maximum = max(maximum, vol)
-            if height[i] < height[j]:
-                i += 1
+            vol = (j-i) * min_height
+            maxx = max(maxx, vol)
+            if height[i] > height[j]:
+                j-=1
             else:
-                j -= 1
-        return maximum
+                i+=1
+        return maxx
+
+
+
+
+
+        # maximum = float("-inf")  # set max to -inf
+        # i, j = 0, len(height) - 1  # initialized 2 pointers at end of array
+
+        # while i < j:  # loop through height array
+        #     # we select min of both -- because overflow
+        #     min_height = min(height[i], height[j])
+        #     vol = (j - i) * min_height  # vol =
+        #     maximum = max(maximum, vol)
+        #     if height[i] < height[j]:
+        #         i += 1
+        #     else:
+        #         j -= 1
+        # return maximum
