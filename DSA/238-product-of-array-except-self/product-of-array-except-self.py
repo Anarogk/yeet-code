20d0afa9a203873e49b0aa9505c99fb3dabc332f
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-16 16:44:41 +0530

    Time: 277 ms (32.01%) | Memory: 25.6 MB (95.64%) - LeetSync

--- DSA/238-product-of-array-except-self/product-of-array-except-self.py
+++ DSA/238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,26 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # n = len(nums)
-        # prefix = 1
-        # postfix = 1
-        # result = [0] *n
-        # for i in range(n):
-        #     result[i] = prefix
-        #     prefix *=nums[i]
-        # for i in range(n-1,-1,-1):
-        #     result[i]*=postfix
-        #     postfix*=nums[i]
-        # return result
-
-
-        nums1 = [1 for i in nums]
-        nums2 = [1 for i in nums]
-        
-        for i in range(1,len(nums)):
-            nums1[i] = nums1[i-1] * nums[i-1]
-        for i in range(len(nums)-2,-1,-1):
-            nums2[i] = nums2[i+1] * nums[i+1]
-        
-        # print(nums1,nums2)
-        nums3 = [i[0]*i[1] for i in zip(nums1,nums2)]
-        return nums3            
+        res = [1] * len(nums)  # [1, 1, 1, 1, 1..]
+        for i in range(1, len(nums)):
+            res[i] = res[i - 1] * nums[i - 1]  # [num[0], nums[1], nums[j]]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
