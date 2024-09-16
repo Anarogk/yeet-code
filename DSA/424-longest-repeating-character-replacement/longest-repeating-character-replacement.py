64a3b0357c29077c5b1f304ee8889056eb995831
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-17 00:32:04 +0530

    Time: 56 ms (99.87%) | Memory: 16.7 MB (38.09%) - LeetSync

--- DSA/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ DSA/424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,10 +6,10 @@ class Solution:
         max_f = 0
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)  # add 1 to count of curr
-            max_f = max(max_f, count[s[r]])
+            max_f = max(max_f, count[s[r]]) # max is max of max or curr count
 
-            if (r - l + 1) - max_f > k:
+            if (r - l + 1) - max_f > k: # if len - max > k we decrement count from left element
                 count[s[l]] -= 1
-                l += 1
+                l += 1 # move left
 
-        return r - l + 1
+        return r - l + 1 # return left
