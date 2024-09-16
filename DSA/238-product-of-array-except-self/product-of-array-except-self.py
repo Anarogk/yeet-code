8102aa4b40ac498499ddac7c7012d447d90a2d2c
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-16 16:50:38 +0530

    Time: 271 ms (48.98%) | Memory: 26.2 MB (35.26%) - LeetSync

--- DSA/238-product-of-array-except-self/product-of-array-except-self.py
+++ DSA/238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,10 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)  # [1, 1, 1, 1, 1..]
-        for i in range(1, len(nums)):
-            res[i] = res[i - 1] * nums[i - 1]  # [num[0], nums[1], nums[j]]
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        # res = [1] * len(nums)  # [1, 1, 1, 1, 1..]
+        # for i in range(1, len(nums)):
+        #     res[i] = res[i - 1] * nums[i - 1]  # [num[0], nums[1], nums[j]]
+        # postfix = 1
+        # for i in range(len(nums) - 1, -1, -1):
+        #     res[i] *= postfix
+        #     postfix *= nums[i]
+        # return res
+
+        n = len(nums)
+
+        prefix = [1] * n  # len(nums)
+        suffix = [1] * n
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] = suffix[i + 1] * nums[i + 1]
+
+        answer = [prefix[i] * suffix[i] for i in range(n)]
+
+        return answer
