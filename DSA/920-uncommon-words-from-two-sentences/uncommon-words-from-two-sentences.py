8c02f25c6916cb789dfd521305ac459bdebd73c4
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-17 08:54:08 +0530

    Time: 37 ms (44.90%) | Memory: 16.6 MB (44.21%) - LeetSync

--- DSA/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
+++ DSA/920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.py
@@ -3,10 +3,8 @@ class Solution:
         sm = defaultdict(int)
         s = s1.strip().split()
         t = s2.strip().split()
-
         for i in s:
             sm[i] = 1+ sm.get(i, 0)
-
         for i in t:
             if i in sm:
                 sm[i] +=1
