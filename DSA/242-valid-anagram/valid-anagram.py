a05ec38b4bb805fe5ed9dfe4a3e3cf0620a81e89
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-17 08:14:58 +0530

    Time: 46 ms (71.69%) | Memory: 16.9 MB (46.46%) - LeetSync

--- DSA/242-valid-anagram/valid-anagram.py
+++ DSA/242-valid-anagram/valid-anagram.py
@@ -1,21 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        dic1 = {}
-        dic2 = {}
-        
+        s1, t1 = defaultdict(int), defaultdict(int)
+
         for i in s:
-            if i in dic1:
-                dic1[i]+=1
-            else:
-                dic1[i] = 1
-        for j in t:
-            if j in dic2:
-                dic2[j]+=1
-            else:
-                dic2[j] = 1
-        for k in dic1:
-            if k not in dic2 or dic1[k] != dic2[k]:
-                return False
-        return True
+            s1[i] = 1 + s1.get(i, 0)
+        for i in t:
+            t1[i] = 1 + t1.get(i, 0)
+        return s1 == t1
