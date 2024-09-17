17696a419fb79ac4ea855f1f1be3caa78d0f544d
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-17 10:49:22 +0530

    Time: 38 ms (57.23%) | Memory: 16.5 MB (34.01%) - LeetSync

--- DSA/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ DSA/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,20 +5,35 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        node1, node2 = list1, list2
-        res = node3 = ListNode(0, None)
+        # node1, node2 = list1, list2
+        # res = node3 = ListNode(0, None)
 
-        while node1 and node2:
-            if node1.val <= node2.val:
-                node3.next = node1
-                node1 = node1.next
+        # while node1 and node2:
+        #     if node1.val <= node2.val:
+        #         node3.next = node1
+        #         node1 = node1.next
+        #     else:
+        #         node3.next = node2
+        #         node2 = node2.next
+        #     node3 = node3.next
+        # if node1 and not node2:
+        #     node3.next = node1
+        # elif node2 and not node1:
+        #     node3.next = node2
+
+        # return res.next
+
+        list3 = new = ListNode(0)
+        while list1 and list2:
+            if list1.val <= list2.val:
+                list3.next = list1
+                list1 = list1.next
             else:
-                node3.next = node2
-                node2 = node2.next
-            node3 = node3.next
-        if node1 and not node2:
-            node3.next = node1
-        elif node2 and not node1:
-            node3.next = node2
-
-        return res.next
+                list3.next = list2
+                list2 = list2.next
+            list3 = list3.next
+        if list1 and not list2:
+            list3.next = list1
+        elif list2 and not list1:
+            list3.next = list2
+        return new.next
