e63c491568d596666f3301d4d90e7960938b3972
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-18 18:31:38 +0530

    Time: 35 ms (51.03%) | Memory: 16.5 MB (13.43%) - LeetSync

--- DSA/70-climbing-stairs/climbing-stairs.py
+++ DSA/70-climbing-stairs/climbing-stairs.py
@@ -1,11 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
-        return self.helper(n, memo)
-    
-    def helper(self, n: int, memo: dict[int, int]) -> int:
         if n == 0 or n == 1:
             return 1
-        if n not in memo:
-            memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
-        return memo[n]
+        prev, curr = 1, 1
+        for _ in range(2, n + 1):
+            temp = curr  # saving curr for prev to move
+            curr = prev + curr  # calculating current
+            prev = temp  # moving prev ahead
+        return curr
