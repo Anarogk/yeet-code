b144519fab29954b15ecca79636c3927b7f5c970
Anurag <85062721+Anarogk@users.noreply.github.com>
2024-09-18 16:26:17 +0530

    Time: 129 ms (41.23%) | Memory: 18.2 MB (21.57%) - LeetSync

--- DSA/283-move-zeroes/move-zeroes.py
+++ DSA/283-move-zeroes/move-zeroes.py
@@ -3,18 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i , j = 0, 1
-        while i < j and j<len(nums):
-            if nums[i] != 0:
-                i+=1
-                j+=1
-                continue
-            if nums[i] ==0 and nums[j]==0:
-                j+=1
-                continue
-            if nums[i] == 0 and nums[j]!= 0:
-                nums[i] , nums[j] = nums[j] , nums[i]
-                i +=1
-                j+=1
-            
-                        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0 and nums[slow] == 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+
+            if nums[slow] != 0:
+                slow +=1        
